ea1b605ea5c412d5e3b2564ff964b595a65754f4
Rudi Thoeni <rudi.thoeni@hotmail.com>
2021-05-26 10:41:47 +0200

    adding filecontroller

---
+++ odh-imageresizer-core/FileController.cs
@@ -0,0 +1,58 @@
+﻿using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Threading;
+using System;
+using AspNetCore.CacheOutput;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using SixLabors.ImageSharp.Formats;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.Formats.Gif;
+using AspNetCore.Proxy;
+
+namespace odh_imageresizer_core
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        public IConfiguration Configuration { get; }
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public FileController(IWebHostEnvironment env, IConfiguration configuration, IHttpClientFactory httpClientFactory)
+        {
+            Configuration = configuration;
+            _httpClientFactory = httpClientFactory;
+        }
+
+        #region Documents
+
+        [CacheOutput(ClientTimeSpan = 0, ServerTimeSpan = 100)]
+        [HttpGet, Route("GetFile/{filename}")]
+        public async Task<IActionResult> GetFile(string filename, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                using var client = _httpClientFactory.CreateClient("buckets");
+                using var stream = await client.GetStreamAsync(filename, cancellationToken);
+                
+                string extension = Path.GetExtension(filename);
+
+                //var stream = await ImageToStream(img, imgrawformat, cancellationToken);
+                return File(stream, extension);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message + " " + ex.InnerException);
+            }
+        }         
+
+        #endregion
+    }
+}
